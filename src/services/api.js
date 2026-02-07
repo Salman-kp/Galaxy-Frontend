30f19d6d035538b21487a4053bb4bd5f253c112f
Salman-kp <salmankp7592@gmail.com>
2026-02-07 14:18:55 +0530

    Updated(config): chnaged api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+console.log("API BASE URL:", import.meta.env.VITE_API_URL);
 
 api.interceptors.response.use(
   (response) => response,
